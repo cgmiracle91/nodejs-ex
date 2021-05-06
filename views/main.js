22c2fc53a697c075bd1daa196e649dcf80841430
cgmiracle91 <81524528+cgmiracle91@users.noreply.github.com>
2021-05-06 19:55:27 -0400

    Update main.js

--- views/main.js
+++ views/main.js
@@ -1,1 +1,6 @@
-let submit = document.getElementByID('submit')
+let submit = document.getElementById('submit')
+
+submit.addEventListener('click', function(){
+  let billAmount = document.getElementById('billAmount');
+  let tipAmount = document.getElementById('tipAmount');
+})
