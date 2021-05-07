f1c4786eb8448cef34f70792a4c6f1ae4c767238
cgmiracle91 <81524528+cgmiracle91@users.noreply.github.com>
2021-05-06 20:06:26 -0400

    Delete main.js

--- views/main.js
+++
@@ -1,8 +0,0 @@
-let submit = document.getElementById('submit')
-
-submit.addEventListener('click', function(){
-  let billAmount = document.getElementById('billAmount').value;
-  let tipAmount = document.getElementById('tipAmount').value;
-  
-  console.log(billAmount);
-})
